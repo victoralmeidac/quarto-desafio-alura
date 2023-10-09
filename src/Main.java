4edc29f7b2152f693c90dcb4451d9f70b5e8ec05
Victor <victoraalmeidac@gmail.com>
2023-10-09 20:25:06 -0300

    Refatoracao com correcao pelo alura

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,29 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonSyntaxException;
+import model.ConsultaCep;
 import model.Endereco;
-import model.EnderecoApi;
+import model.GeradorDeArquivo;
 
-import java.io.FileWriter;
+
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException{
 
         Scanner leitura = new Scanner(System.in);
-        String busca = "";
-        List<Endereco> endereco = new ArrayList<>();
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        ConsultaCep consultaCep = new ConsultaCep();
 
-        while (!busca.equalsIgnoreCase("sair")) {
-            System.out.println("Digite um CEP com 8 digitos: ");
-            busca = leitura.nextLine();
-            if (busca.equalsIgnoreCase("sair")) {
-                break;
-            }
-            String url = "https://viacep.com.br/ws/" + busca.replace("-", "") + "/json/";
-            try {
-                HttpClient client = HttpClient.newHttpClient();
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create(url))
-                        .build();
-                HttpResponse<String> response = client
-                        .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Digite um número de CEP para consulta:");
+        var cep = leitura.nextLine();
 
-                String json = response.body();
-
-                EnderecoApi enderecoPesquisado = gson.fromJson(json, EnderecoApi.class);
-                Endereco meuEndereco = new Endereco(enderecoPesquisado);
-                endereco.add(meuEndereco);
-            } catch (JsonSyntaxException e){
-                System.out.println("Digite um cep valido apenas com caracteres numericos até 8 digitos ");
-            }
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
         }
-        FileWriter escrita = new FileWriter("cep.json");
-        escrita.write(gson.toJson(endereco));
-        escrita.close();
-        System.out.println("O programa finalizou");
 
     }
 }
